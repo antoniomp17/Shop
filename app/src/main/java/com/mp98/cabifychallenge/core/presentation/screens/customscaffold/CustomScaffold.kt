847c3294f25c0772096f97a1049f5fa35ff4b5b4
antoniomp17 <antoniomoraleda17@gmail.com>
2024-11-01 22:21:46 +0100

    #12 -> Bottom Bar

--- app/src/main/java/com/mp98/cabifychallenge/core/presentation/screens/customscaffold/CustomScaffold.kt
+++ app/src/main/java/com/mp98/cabifychallenge/core/presentation/screens/customscaffold/CustomScaffold.kt
@@ -1,8 +1,11 @@
 package com.mp98.cabifychallenge.core.presentation.screens.customscaffold
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,20 +14,27 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.BarChart
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.ShoppingBag
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mp98.cabifychallenge.core.presentation.screens.navigation.NavigationRoute
 import com.mp98.cabifychallenge.core.presentation.viewmodels.ProductCartViewModel
 import com.mp98.cabifychallenge.core.utils.dynamicPadding
+import com.mp98.cabifychallenge.core.utils.scalableText
+import com.mp98.cabifychallenge.core.utils.toCurrencyFormat
 
 @Composable
 fun CustomScaffold(
@@ -39,6 +49,20 @@ fun CustomScaffold(
 
     Scaffold(
         modifier = modifier,
+        bottomBar = {
+            BottomAppBar{
+                Row (modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically){
+                    Text(
+                        text = "TOTAL: ${state.cart.total.toCurrencyFormat()}",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = scalableText(18.sp)
+                    )
+                }
+            }
+        },
         topBar = {
             TopBar(productCartViewModel = productCartViewModel){
                 productCartViewModel.changeScreen(NavigationRoute.CartListScreen)
@@ -55,7 +79,7 @@ fun CustomScaffold(
                             productCartViewModel.changeScreen(NavigationRoute.ProductListScreen)
                             onChangeToProducts()
                         },
-                        modifier = Modifier.align(Alignment.BottomEnd)
+                        modifier = Modifier.align(Alignment.BottomEnd).offset(y = 60.dp)
                     ) {
                         Icon(
                             imageVector = Icons.Rounded.ShoppingBag,
