11dc4d097cc94b1a68a693591fd789d305f0ab97
antoniomp17 <antoniomoraleda17@gmail.com>
2025-02-14 18:30:12 +0100

    Open external app with url when session is ready

--- app/src/main/java/com/mp98/shop/ShopMainScreen.kt
+++ app/src/main/java/com/mp98/shop/ShopMainScreen.kt
@@ -1,8 +1,16 @@
 package com.mp98.shop
 
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat.startActivity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.mp98.shop.core.presentation.screens.customscaffold.CustomScaffold
@@ -12,7 +20,25 @@ import com.mp98.shop.core.presentation.viewmodels.ProductCartViewModel
 import com.mp98.shop.ui.theme.ShopTheme
 
 @Composable
-fun ShopMainContent(productCartViewModel: ProductCartViewModel = hiltViewModel()){
+fun ShopMainContent(productCartViewModel: ProductCartViewModel = hiltViewModel()) {
+    val state by productCartViewModel.productsCartState.collectAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(state.sessionUrl) {
+        state.sessionUrl?.let { url ->
+            val deepLinkUri = Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, deepLinkUri).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+
+            if (intent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(intent)
+            } else {
+                Log.e("DeepLink", "No app found to handle the deep link")
+            }
+        }
+    }
+
     ShopTheme{
 
         val navHostController = rememberNavController()
